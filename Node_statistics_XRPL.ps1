6b6075356e138611b16b7ec7a1492e0119d88fa4
Jon Nilsen <40994635+jonaagenilsen@users.noreply.github.com>
2022-09-23 13:59:46 +0200

    Update Node_statistics_XRPL.ps1

--- Node_statistics_XRPL.ps1
+++ Node_statistics_XRPL.ps1
@@ -25,19 +25,22 @@ if ($Servers) {
   
   foreach ($Server in $($Servers.nodes | Select-Object version -Unique)) {
     $ServerCount = ($Servers.nodes | Where-Object { $_.version -match "$($Server.version)" }).count
+    $CurrentVersionCount = ($Servers.nodes | Where-Object { $_.version -match "$($Server.version)" }).count
+    $CurrentVersionPercent = [math]::Round(($CurrentVersionCount * 100 / $Servers.total),1)
 
     $Object = [pscustomobject]@{
        version = $($Server.version)
        count = $ServerCount
+       percent = $CurrentVersionPercent
     }
     $ServerStatistics += $Object
   }
 
   Write-Host "Servers sorted by version" -NoNewline
-  $ServerStatistics | Select-Object * | Sort-Object version -Descending | ft -HideTableHeaders
+  $ServerStatistics | Select-Object * | Sort-Object version -Descending | ft #-HideTableHeaders
   
   Write-Host "Servers sorted by count" -NoNewline
-  $ServerStatistics | Select-Object * | Sort-Object count -Descending | ft -HideTableHeaders
+  $ServerStatistics | Select-Object * | Sort-Object count -Descending | ft #-HideTableHeaders
 }
 else {
   Write-Host "*Display old man yelling at clouds"
